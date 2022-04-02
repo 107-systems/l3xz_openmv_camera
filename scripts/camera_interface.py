0a7355583d2b0f15ad43546190d1db3781faef3c
wuehr1999 <jonas.wuehr@t-online.de>
2022-04-02 19:49:39 +0100

    little refactoring

--- scripts/camera_interface.py
+++ scripts/camera_interface.py
@@ -1,27 +1,62 @@
+import rospy
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo 
+
 import cv2
+import numpy as np
+
 from openmv.tools import pyopenmv
 
-CAM_SCRIPT = """
-import sensor, image, time
-sensor.reset()
-sensor.set_pixformat(sensor.RGB565)
-sensor.set_framesize(sensor.QVGA)
-sensor.skip_frames(time = 2000)
-clock = time.clock()
-while(True):
-  img = sensor.snapshot()
-  sensor.flush()
-"""
+class FrameDump:
 
-class OpenmvDumper:
+  def __init__(self, width, height, pixels):
+    self._width = width
+    self._height = height
+    self._pixels = pixels
+    
+  @property 
+  def width(self):
+    return self._width
 
-  def __init__(self, port = "/dev/ttyACM0"):
+  @property
+  def height(self):
+    return self._height
+
+  @property
+  def pixels(self):
+    return self._pixels
+
+  def fill_img_msg(self, msg = None):
+    if msg is None:
+      msg = Image()
+    msg.header.stamp = rospy.Time.now()
+    msg.encoding = "bgr8"
+    msg.step = 3 * self._width 
+    msg.width = self._width 
+    msg.height = self._height
+    msg.data = self._pixels.tostring()
+    return msg
+
+  def fill_info_msg(self, msg = None):
+    if msg is None:
+      msg = CameraInfo()
+    msg.width = self._width
+    msg.height = self._height
+    return msg
+
+class OpenMvInterface:
+
+  def __init__(self, port, script_file):
     pyopenmv.disconnect()
     pyopenmv.init(port, baudrate = 921600, timeout = 0.050)
     pyopenmv.set_timeout(1 * 2)
     pyopenmv.stop_script()
     pyopenmv.enable_fb(True)
-    pyopenmv.exec_script(CAM_SCRIPT)
+    script = open(script_file, "r")
+    script_str = script.read()
+    script.close()
+    pyopenmv.exec_script(script_str)
+
   def __del__(self):
     pyopenmv.stop_script()
     pyopenmv.disconnect()
@@ -29,9 +64,6 @@ class OpenmvDumper:
   def dump(self):
     fb =  pyopenmv.fb_dump()
     if fb is not None:
-      print(fb[0])
-      print(fb[1])
-      img = fb[2]
-      return img
+      return FrameDump(fb[0], fb[1], fb[2])
     else:
       return None
--- scripts/l3xz_openmv_camera_node.py
+++ scripts/l3xz_openmv_camera_node.py
@@ -2,33 +2,55 @@
 #-*- coding: utf-8 -*-
 
 import rospy
-
+import roslib
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo 
 
 import cv2
 import numpy as np
-
-from camera_interface import OpenmvDumper
+from camera_interface import OpenMvInterface
 
 import time
 
-d = OpenmvDumper()
-rospy.init_node('openmv')
-pub = rospy.Publisher("img", Image, queue_size = 1)
-while True:
-  img = d.dump()
-  r = rospy.Rate(100)
-  if img is not None:
-    cv2.imshow("img", img)
-    cv2.waitKey(1)
-    msg = Image()
-    msg.header.stamp = rospy.Time.now()
-    msg.encoding = "bgr8"
-    msg.step = 3 * 320
-    msg.width = 320
-    msg.height = 240
-    msg.data = img.tostring()
-    print("publish")
-    pub.publish(msg)
+def main():
+  rospy.init_node('openmv')
 
-  r.sleep()
+  frame_id = rospy.get_param("~frame_id", "odom")
+
+  show = rospy.get_param("~show_image", False)
+
+  rate = rospy.Rate(rospy.get_param("~rate_hz", 10))
+
+  omv_cam = OpenMvInterface(rospy.get_param("~port", "/dev/ttyACM0"),
+     rospy.get_param("~camera_script", "/home/pi/catkin_ws/src/l3xz_openmv_camera/src/interfacecamera_script.py"))
+ 
+  img_name = rospy.get_param("~image_topic", "image_color")
+  pub_image = rospy.Publisher(img_name,
+          Image, queue_size = rospy.get_param("~image_queue", 1))
+  pub_info = rospy.Publisher(rospy.get_param("~info_topic", "camera_info"),
+          CameraInfo, queue_size = rospy.get_param("~image_queue", 1))
+
+  img_msg = Image()
+  img_msg.header.frame_id = frame_id
+
+  info_msg = CameraInfo()
+  info_msg.header.frame_id = frame_id
+
+  while not rospy.is_shutdown():
+    
+    framedump = omv_cam.dump()
+    if framedump is not None:
+      if show:
+        cv2.imshow(img_name, framedump.pixels)
+        cv2.waitKey(1)
+      time = rospy.Time.now()
+      img_msg.header.stamp = time 
+      pub_image.publish(framedump.fill_img_msg(img_msg))
+
+      info_msg.header.stamp = time
+      pub_info.publish(framedump.fill_info_msg(info_msg))
+
+    rate.sleep()
+
+if __name__ == '__main__':
+  main()
